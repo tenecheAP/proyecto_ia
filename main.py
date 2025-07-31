fbf0661ebb2ade30b5eb74279f9887ccb14ef2e3
tenecheAP <tenechepablo@gmail.com>
2025-07-30 21:24:31 -0500

    se creó el archivo amin.py y se hizo que se guardara los stopwords a una carpeta local en el disco c:

---
+++ main.py
@@ -0,0 +1,18 @@
+#importar la libreria nltk
+import nltk
+
+#importar las stopwords
+from nltk.corpus import stopwords
+
+#descargar el paquete de stopwords
+nltk.download('stopwords')
+
+#definir la ruta donde se almacenaran los datos descargados de nltk
+nltk.data.path.append(r'C:\Users\tenec\AppData\Roaming\nltk_data')
+
+
+
+
+
+
+
